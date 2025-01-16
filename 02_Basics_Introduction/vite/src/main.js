cb935c2a86f99992d3d9476d80f4ccaec95b256b
Syed Hisham Shah <syedhishamshah27@gmail.com>
2025-01-16 08:00:15 +0500

    add: Vite App with Three.js

---
+++ 02_Basics_Introduction/vite/src/main.js
@@ -0,0 +1,53 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+const canvas = document.getElementById('canvas');
+
+// 1. Create the Scene
+const scene = new THREE.Scene();
+scene.background = new THREE.Color('#F0F0F0');
+
+// 2. Add the camera
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 5;
+
+// 3. Create and add the an object
+const dodecahedronGeometry = new THREE.DodecahedronGeometry();
+const dodecahedronMaterial = new THREE.MeshLambertMaterial({color: '#468585', emissive: '#468585'});
+const dodecahedron = new THREE.Mesh(dodecahedronGeometry, dodecahedronMaterial);
+
+const boxGeometry = new THREE.BoxGeometry(2, 0.1, 2);
+const boxMaterial = new THREE.MeshStandardMaterial({color: '#B4B4B3', emissive: '#B4B4B3'});
+const box = new THREE.Mesh(boxGeometry, boxMaterial);
+box.position.y = -1.5;
+
+scene.add(dodecahedron);
+scene.add(box);
+// 4. Add lightning
+const light = new THREE.SpotLight(0x9CDBA6, 10);
+light.position.set(1, 1, 1);
+scene.add(light);
+
+// 5. Setup the renderer
+const renderer = new THREE.WebGLRenderer({canvas});
+renderer.setSize(window.innerWidth , window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
+
+// 6. Add orbit controls
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.05;
+controls.enableZoom = true;
+controls.enablePan = true;
+
+// 7. Add animations
+function animate() {
+  requestAnimationFrame(animate);
+  dodecahedron.rotation.x += 0.01;
+  dodecahedron.rotation.y += 0.01;
+
+  box.rotation.y += 0.005;
+  controls.update();
+  renderer.render(scene, camera);
+}
+
+animate();
